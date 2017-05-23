d2749ee9c3565cfa0dfea2f19be25bc13d28ac71
Maison Chaves <maison.chaves@p4sglobal.com>
2017-05-23 16:16:53 -0300

    initi mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+'use strict';
+
 var Koa = require('koa');
 var Router = require('koa-router');
+const MongoClient = require('mongodb').MongoClient;
 
 var app = new Koa();
 var router = new Router();
+var db;
 
 router.get('/', function (ctx) {
   ctx.body = 'Hello from Koajs using router';
@@ -12,8 +16,27 @@ router.get('/date', function (ctx) {
   ctx.body = new Date().toLocaleString();
 });
 
+router.get('/chats', function (ctx, next) {
+  console.log(ctx.body);
+  db.collection('chats').find().toArray(function (err, results) {
+    if (err) return console.log(err)
+
+    console.log(results);
+    ctx.body = "Results";
+    console.log(ctx.body);
+    next();
+  });
+  console.log(ctx.body);
+});
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(3000);
+//app.listen(3000);
+
+MongoClient.connect('mongodb://maison:maison@ds151451.mlab.com:51451/robos-im', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000);
+});
