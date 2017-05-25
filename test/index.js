bab243015acccde47fecc977e6c52c637bbc0386
Maison Chaves <maison.chaves@p4sglobal.com>
2017-05-24 23:19:53 -0300

    include chai

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 import http from 'http';
 import assert from 'assert';
+var expect = require('chai').expect;
 
 import '../app.js';
 
 describe('Example Node Server', () => {
     it('should return 200', done => {
         http.get('http://127.0.0.1:3000', res => {
-            assert.equal(200, res.statusCode);
+            expect(res.statusCode).to.equal(200);
             done();
         });
     });
@@ -20,6 +21,7 @@ describe('Example Node Server', () => {
 
             res.on('end', () => {
                 assert.equal('Hello from Koajs using router', data);
+                expect(data).to.equal('Hello from Koajs using router');
                 done();
             });
         });
